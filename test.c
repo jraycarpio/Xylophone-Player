2c6677f0b9887d2a82e1fae29c3a50bb7383038e
jraycarpio <jraycarpio@gmail.com>
2021-01-25 20:15:34 -0800

    Update test.c

--- test.c
+++ test.c
@@ -1,30 +1,35 @@
-// Test location servo
+// Test servos
 
 #include <Servo.h>
 
 Servo location;
 int pos;
+int newLoc;
+String input;
+String testInput = "test";
 
 void moveLoc(int desired) {
-  desired = pos + desired;
-  desired = pos;
   location.write(desired);
-  Serial.print("Moving ");
-  Serial.print(desired);
-  Serial.println(" steps");
+  pos = desired;
+  Serial.print("\nMoving to ");
+  Serial.print(pos);
+  Serial.println(" step location");
 }
 
 
 // test if movement of note is correct
 void testNote() {
-  moveLoc(24);
+  moveLoc(90);
+  delay(1000);
+  
+  moveLoc(90-24);
   delay(2000);
-  moveLoc(24);
+  moveLoc(90-48);
   delay(2000);
 
-  moveLoc(-24);
+  moveLoc(90-24);
   delay(2000);
-  moveLoc(-24);
+  moveLoc(90);
   delay(2000);
 }
 
@@ -42,22 +47,43 @@ void setup() {
   Serial.println("SYSTEM START\n");
   location.attach(5);
 
+  pos = 0;
+
   moveLoc(90);
 
-  delay(2000);
-
+  delay(1000);
 }
 
 void loop() {
 
-  Serial.println("Begin loop...\n\n");
-  Serial.print("Current position is ");
-  Serial.println(pos);
+//  Serial.println("Begin loop...\n\n");
+
   
-  testNote();
+  Serial.print("\nCurrent position is ");
+  Serial.println(pos);
+  Serial.println("\nWhat position would you like to move to?");
+
+  while (Serial.available() == 0) {}
+  // read the incoming byte:
+  input = Serial.readString();
+  if (input.equals(testInput)) {
+    Serial.println("you wrote test");
+    testNote();
+  } else {
+    newLoc = input.toInt();
+    moveLoc(newLoc);  
+  }
+  
+
+  delay(1000);
 
   
   
-  Serial.println("Restarting loop in 5 seconds");
-  delay(5000);
+//  Serial.println("Restarting loop in 3...");
+//  delay(1000);
+//  Serial.println("Restarting loop in 2...");
+//  delay(1000);
+//  Serial.println("Restarting loop in 1...");
+//  delay(1000);
 }
+ 
